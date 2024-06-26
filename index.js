7d39142f59c27b5513a9a456d7902f91db9e5cd0
meirman_is_creator <sarsenbaymeyirman@gmail.com>
2024-06-26 18:57:17 +0500

    feat: fix cookie

--- index.js
+++ index.js
@@ -84,6 +84,7 @@ app.post("/login", async (req, res) => {
               secure: process.env.NODE_ENV === 'production', 
               maxAge: 3600000, 
               path: '/',
+              domain:"airbnb-clone-front-end.vercel.app",
               sameSite: 'Strict'
             }).json(userDoc);
           }
