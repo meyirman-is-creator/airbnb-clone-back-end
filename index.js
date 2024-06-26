ec02071d42f61b159eac894c174959abe0acb4b1
meirman_is_creator <sarsenbaymeyirman@gmail.com>
2024-06-26 12:12:26 +0500

    i updated

--- index.js
+++ index.js
@@ -205,6 +205,7 @@ app.post("/places", (req, res) => {
 
 app.get("/user-places", (req, res) => {
   const { token } = req.cookies;
+
   jwt.verify(token, jwtSecret, {}, async (err, userData) => {
     const { id } = userData;
     res.json(await Place.find({ owner: id }));
